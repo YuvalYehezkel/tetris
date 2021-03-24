6d1b4e03d798098194915ad473a40a282a7efdf2
YuvalYehezkel <81328996+YuvalYehezkel@users.noreply.github.com>
2021-03-25 00:37:09 +0200

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -7,11 +7,11 @@ Board::Board()
 }
 void Board::initBoard()
 {
-	for (int i = 0; i <= 2*ROWS + 1; i++)
+	for (int i = 0; i <= 2*ROWS + 2; i++)
 	{
 		for (int j = 0; j <= COLS + 1; j++)
 		{
-			if (j == 0 || j == COLS + 1 || j == 2*COLS+1 || i == 0 || i == ROWS + 1 || i == 2*ROWS + 1)
+			if (j == 0 || j == COLS + 1  || i == 0 || i == ROWS + 1 || i == 2*ROWS + 2)
 			{
 				gotoxy(i, j);
 				cout << '#';
