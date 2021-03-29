390016df3443c7fd9065482e94ce4057caa6abbc
YuvalYehezkel <81328996+YuvalYehezkel@users.noreply.github.com>
2021-03-30 02:21:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,18 @@
 #include "Figure.h"
 #define ESC 27
 #include <conio.h>
+#include "TheGame.h"
+
+
 using namespace std;
 
 //
 
 int main()
 {
+
 	Board board1;
-	gotoxy(10, 10);// ברגע שיש גו טו איקס ווי עם בורד, התוכנית עפה, בלי שניהם ביחד עובד
+	//gotoxy(10, 10);// ברגע שיש גו טו איקס ווי עם בורד, התוכנית עפה, בלי שניהם ביחד עובד
 	Figure f;
 	//f.setBoard(&board1); // לא יודעת אם באמת צריך
 	//f.printFigure();
@@ -17,38 +21,31 @@ int main()
 	/*gotoxy(1, 1);
 	cout << getch;*/
 
-	////write text in 4 corners of the screan
-	//clrScr();
-	//gotoxy(10, 10);
-	//std::cout << "at 10 10";
-	//gotoxy(10,20);
-	//std::cout << "at 10 20";
-	//gotoxy(20, 10);
-	//std::cout << "at 20 10";
-	//gotoxy(20, 20);
-	//std::cout << "at 20 20";
-	//
+
 	//clrScr();
 	//_flushall();
 
-	int x = 1, y = 1;
+	int x = 7, y = 1;
 
-	while ((!_kbhit()||_getch() != ESC) && y<13)
+	while ((!_kbhit() || _getch() != ESC))
 	{
-		//if (!_kbhit())
-		//{
-			gotoxy(x, y);
-			f.printFigure();
-			cout << endl<<endl;
-			Sleep(1000);
-			gotoxy(x, y);
-			if(y<=11)
-				cout << "    " << endl<<endl;
-			/*++x;
-			if (x > 79)
-				x = 1;*/
-			++y;
-			
+
+		gotoxy(x, y);
+
+
+		f.printFigure();
+
+		Sleep(1000);
+		gotoxy(x, y);
+		if (y <= 11)
+			cout << "    " << endl << endl;
+		//++x;
+		++y;
+		if (y > 12)
+			y = 1;
+
+
+
 		//}
 		//else if (_getch() == 'a' || _getch() == 'A' || _getch() == 'j' || _getch() == 'J')
 		//{
@@ -59,5 +56,10 @@ int main()
 
 		//}
 	}
+	TheGame g; // ניסיון
+	g.ShowInstructions();
+
+
+	cout << endl << endl;
 	return 0;
 }
