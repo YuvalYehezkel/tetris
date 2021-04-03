f1d95edf3881ea82c02aafc92ef29a7aeb593b14
YuvalYehezkel <81328996+YuvalYehezkel@users.noreply.github.com>
2021-04-04 00:52:13 +0300

    Update Figure.cpp

--- Figure.cpp
+++ Figure.cpp
@@ -2,38 +2,160 @@
 
 void Figure::Rotate()
 {
-
+	//if(direction=='w')
 }
 void Figure::RightOrLeftOrdown()
 {
 
 }
-void Figure::printFigure()
+void Figure::printFigure(Point* p)
 {
 
-	for (int i = 0; i < rows; i++)
+	for (int i = 0; i < SIZE; i++)
 	{
-		
-		if (figure1[i] == 1)
-			cout << '*';
-		else
-			cout << ' ';
-	
+		for (int j = 0; j < SIZE; j++)
+		{
+			if ((FigureBody[i][j] == 1) && !(*p).getIsBusy())
+			{
+				gotoxy((*p).getX(), (*p).gety());
+				cout << '#';
+				int x = (*p).getX();//צריך עידכון לgetIsBusy
+				x++;
+			}
+			else
+			{
+			
+				cout << ' ';
+			}
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void Figure::initFigureBySerialNum()
+{
+	switch (serial_num_of_figure) //1-7
+	{
+	case SHAPES::l:
+	{
+		for (int i = 0; i < SIZE; i++)
+
+			for (int j = 0; j < SIZE; j++)
+			{
+				if (i == 0)
+					FigureBody[i][j] = 1;
+				else
+					FigureBody[i][j] = 0;
+			}
+		break;
+
+	}
+
+	case SHAPES::L:
+	{
+		for (int i = 0; i < 4; i++)
+
+			for (int j = 0; j < 4; j++)
+			{
+				if ((i == 1 && j != 0) || (i == 0) && (j == 1))
+					FigureBody[i][j] = 1;
+				else
+					FigureBody[i][j] = 0;
+			}
+		break;
+	}
+	case SHAPES::J:
+	{
+		for (int i = 0; i < 4; i++)
+
+			for (int j = 0; j < 4; j++)
+			{
+				if ((i == 1 && j != 0) || (i == 0 && j == 3))
+					FigureBody[i][j] = 1;
+				else
+					FigureBody[i][j] = 0;
+			}
+		break;
+	}
+	case SHAPES::O:
+	{
+		for (int i = 0; i < 4; i++)
+
+			for (int j = 0; j < 4; j++)
+			{
+				if ((i == 0 && j != 0 && j != 3) || (i == 1 && j != 0 && j != 3))
+					FigureBody[i][j] = 1;
+				else
+					FigureBody[i][j] = 0;
+			}
+		break;
+	}
+	case SHAPES::Z:
+	{
+		for (int i = 0; i < 4; i++)
+
+			for (int j = 0; j < 4; j++)
+			{
+				if ((i == 0 && j != 0 && j != 3) || (i == 1 && j != 0 && j != 1))
+					FigureBody[i][j] = 1;
+				else
+					FigureBody[i][j] = 0;
+			}
+		break;
+	}
+
+
+	case SHAPES::S:
+	{
+		for (int i = 0; i < 4; i++)
+
+			for (int j = 0; j < 4; j++)
+			{
+				if ((i == 0) && (j == 2 || j == 3) || (i == 1) && (j == 1 || j == 2))
+					FigureBody[i][j] = 1;
+				else
+					FigureBody[i][j] = 0;
+			}
+		break;
+	}
+
+	case SHAPES::RT:
+	{
+		for (int i = 0; i < 4; i++)
+
+			for (int j = 0; j < 4; j++)
+			{
+
+				if ((i == 0 && j == 2) || (i == 1 && j != 0))
+					FigureBody[i][j] = 1;
+				else
+					FigureBody[i][j] = 0;
+			}
+		break;
+	}
+
+	}
+
+}
+
+void Figure::deleteFigure()
+{
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < SIZE; j++)
+		{
+			if (FigureBody[i][j] == 1)
+				cout << ' ';
+			/*else
+				cout << ' ';*/
+		}
+		cout << endl;
 	}
 }
 
-void Figure::initFigure()
-{
-	
-	figure1[0] = 1;
-	figure1[1] = 1;
-	figure1[2] = 1;
-	figure1[3] = 1;
-
-
-
-}
-void Figure::setBoard(Board* b1)//לא יודעת אם באמת צריך
+void Figure::setBoard(Board* b1)//
 {
 	b = b1;
 }
